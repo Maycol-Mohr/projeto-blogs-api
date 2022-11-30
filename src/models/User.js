025270bb7c5f5fd7991b286a927d5ede83cf16a8
Maycol-Mohr <maycolmohr@yahoo.com.br>
2022-11-30 22:19:06 +0100

    tentando exercicio 2

---
+++ src/models/User.js
@@ -0,0 +1,20 @@
+// src/models/user.model.js
+
+const UserModel = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },  
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+        timestamps: false,
+        tableName: 'users',
+        underscored: true,
+      });
+  
+    return User;
+  };
+  
+  module.exports = UserModel;
