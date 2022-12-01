f7b0781603eb1c6ceede9551b0cedeca1f2a1264
Maycol-Mohr <maycolmohr@yahoo.com.br>
2022-12-01 19:47:20 +0100

    tentando exercicio 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,33 @@
+// src/models/UserBook.js
+
+// const { DataTypes } = require("sequelize/types");
+
+module.exports = (sequelize, DataTypes) => {
+    const PostCategory = sequelize.define('PostCategory',
+      {postId: { type: DataTypes.INTEGER, primaryKey: true }, 
+      categoryId: { type: DataTypes.INTEGER, primaryKey: true }},
+      {
+        timestamps: false,
+        underscored: true, 
+        tableName: 'posts_categories'
+      },
+    );
+
+    PostCategory.associate = (models) => {
+      models.Category.belongsToMany(models.BlogPost, {
+        as: 'blogPosts',
+        through: PostCategory,
+        foreignKey: 'categoryId', 
+        otherKey: 'postId', 
+      });
+      models.BlogPost.belongsToMany(models.Category, {
+        as: 'categories',
+        through: PostCategory,
+        foreignKey: 'postId',  
+        otherKey: 'categoryId',
+      });
+    };
+  
+      return PostCategory;
+  };
+  
